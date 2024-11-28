673873e6bb8aa9d78044975514b9de0e7495c069
cyberneel <neelesh.chevuri25@gmail.com>
2024-11-28 00:18:57 -0600

    Added PWM to motor controller
    Main reason is to control the intensity of the wakeup vibrations

--- src/displayapp/DisplayApp.cpp
+++ src/displayapp/DisplayApp.cpp
@@ -406,8 +406,9 @@ void DisplayApp::Refresh() {
         } else {
           LoadNewScreen(Apps::Sleep, DisplayApp::FullRefreshDirections::None);
         }
-        motorController.RunForDuration(infiniSleepController.gradualWakeVibrationDurations[-1 + infiniSleepController.gradualWakeStep]);
-
+        //motorController.RunForDuration(infiniSleepController.gradualWakeVibrationDurations[-1 + infiniSleepController.gradualWakeStep]);
+        motorController.GradualWakeBuzz();
+        
         infiniSleepController.UpdateGradualWake();
 
         NRF_LOG_INFO("Gradual wake triggered");
