c33206b0b1b554c103b4c946ecffff67f6cc87db
Titus Kendzorra <titus@kendzorra.de>
2025-07-25 17:42:53 +0200

    passkeyLabel -> passkeyValue cuz why not

--- src/displayapp/screens/PassKey.cpp
+++ src/displayapp/screens/PassKey.cpp
@@ -4,11 +4,11 @@
 using namespace Pinetime::Applications::Screens;
 
 PassKey::PassKey(uint32_t key) {
-  passkeyLabel = lv_label_create(lv_scr_act(), nullptr);
-  lv_obj_set_style_local_text_color(passkeyLabel, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_YELLOW);
-  lv_obj_set_style_local_text_font(passkeyLabel, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_42);
-  lv_label_set_text_fmt(passkeyLabel, "%06u", key);
-  lv_obj_align(passkeyLabel, nullptr, LV_ALIGN_CENTER, 0, -20);
+  passkeyValue = lv_label_create(lv_scr_act(), nullptr);
+  lv_obj_set_style_local_text_color(passkeyValue, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_YELLOW);
+  lv_obj_set_style_local_text_font(passkeyValue, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_42);
+  lv_label_set_text_fmt(passkeyValue, "%06u", key);
+  lv_obj_align(passkeyValue, nullptr, LV_ALIGN_CENTER, 0, -20);
 }
 
 PassKey::~PassKey() {
